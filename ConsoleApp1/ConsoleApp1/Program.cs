76dc08909df5608ff3ffb263467361776a247a72
LiksK <144459112+LiksK@users.noreply.github.com>
2024-04-23 14:45:04 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+/*networkInterface = NetworkInterface.GetAllNetworkInterfaces()
+IPInterfaceProperties properties = networkInterface.GetIPProperties();
+Console.WriteLine(properties.UnicastAddresses)
+*/
+Console.WriteLine("Устройства, подключенные к сети:");
+
+foreach (NetworkInterface networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+{
+    IPInterfaceProperties properties = networkInterface.GetIPProperties();
+    foreach (UnicastIPAddressInformation address in properties.UnicastAddresses){
+        if (address.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork){
+            Console.WriteLine($"Имя устройства: {networkInterface.Name}");
+            Console.WriteLine($"IP-адрес: {address.Address}");
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+
+
+Console.WriteLine("Измерение скорости интернета...");// вывод тектса 
+
+
+WebClient client = new WebClient(); // создание нового вебклиента
+
+
+Stopwatch stopwatch = new Stopwatch(); // создание нового класса для измерения времени при запросе на сервер
+stopwatch.Start();
+
+
+byte[] data = client.DownloadData("http://www.google.com"); // Отправляем запрос на сервер для получения данных
+
+stopwatch.Stop(); // конец измерения 
+
+double elapsedTimeInSeconds = stopwatch.Elapsed.TotalSeconds; // получение затраченных секуед
+double dataSizeInBytes = data.Length; // получение обьема данных 
+double speedInMbps = dataSizeInBytes / elapsedTimeInSeconds * 8 / 1024 / 1024; // делим обьем на скорость * 8 /1024 /1024
+
+Console.WriteLine($"Скорость интернета: {speedInMbps:F2} Mbps"); //вывод 
+
+client.Dispose();// удаление ресов занимаемыми вебклиентом
